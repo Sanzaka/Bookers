cb7439a3027c66feeb8295d58ec9b181976b5377
Sanzaka <equation0815@icloud.com>
2021-06-24 07:37:33 +0000

    [add]topとindexの、htmlとｃｓｓを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :books
-  
+  resources :books, only: [:index, :edit, :show]
+  root :to => "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
