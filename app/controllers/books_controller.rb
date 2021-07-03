4f14c31e0f76866ea2f44437153a46fa10c8fc53
Sanzaka <equation0815@icloud.com>
2021-07-03 05:18:37 +0000

    [end]Rspecエラー無し

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,46 @@
 class BooksController < ApplicationController
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), flash:{success:"Book was successfully created."}
+    else
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), flash:{success:"Book was successfully updated."}
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, flash:{success:"Book was successfully destroyed."}
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index, :edit, :show]
+  resources :books
+  # get "books" => "books#index", as:"books"
+  # post "book" => "books#create"
+  # get "book/:id" => "books#show", as:"show_book"
+  # get "book/:id/edit" => "books#edit", as:"edit_book"
+  # patch "book/:id" => "books#update", as:"update_book"
+  # delete "book/:id" => "books#destroy", as:"destroy_book"
   root :to => "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
